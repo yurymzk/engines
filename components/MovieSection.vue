3ba8029fc0480cf32a8533e5d98d93da1450b534
yurymzk <yamazaki.engines@gmail.com>
2019-02-20 12:02:55 +0900

    背景動画の調整

--- components/MovieSection.vue
+++ components/MovieSection.vue
@@ -2,8 +2,9 @@
     <div id="MovieSection">
         <div class="MovieWindowWrapper">
                 <div id="video">
-                <iframe src="https://www.youtube.com/embed/S8W4yDXQ4Lc?playlist=S8W4yDXQ4Lc&autoplay=1&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/S8W4yDXQ4Lc?autoplay=1&loop=1&mute=1&showinfo=0&controls=0&playlist=S8W4yDXQ4Lc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
+                <div id="video-overlay"></div>
             <div class="logo"></div>                
         </div>
     </div>
@@ -14,8 +15,7 @@
 <style>
 #MovieSection {
     height: 100vh;
-    width: 100vw;
-    z-index: -20;
+    width: 200vw;
 }
 #MovieSection .MovieWindowWrapper {
     height: 100vh;
@@ -23,23 +23,26 @@
     width: 100%;
 }
 
-#MovieSection #video {
-    background: url('/video/video_bg.jpg') no-repeat;
-    background-attachment: fixed;
-    background-position: center center;
-
+#MovieSection #video,
+#MovieSection #video-overlay {
+    /* wrapperサイズに大路て、leftの位置をJQueryで指定するため、positionはabsoluteにします。 */
+    position:fixed;
+    top: 0;
+    left: 0; 
+    
     /* 以下、画面いっぱいにしうるためのCSS設定 */
     min-height: 100%;
     min-height: 100vh;
     min-width: 100%;
     min-width: 100vw;
+}
 
-    /* wrapperサイズに大路て、leftの位置をJQueryで指定するため、positionはabsoluteにします。 */
-    position:fixed;
-    top: 0;
-    left: 0;
+#MovieSection #video {
+    z-index:-2;
+}
 
-    
+#MovieSection #video-overlay {
+    z-index: -1;
 }
 
 #MovieSection #video iframe{
